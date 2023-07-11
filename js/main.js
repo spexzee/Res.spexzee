45e65f983bf70eaa51ea71e4f2adf3b36dea7daa
spexzee <abub5979@gmail.com>
2023-07-11 22:02:02 +0530

    start

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let n = document.querySelector('.navBar')
+function navMenu() {
+    if (n.style.display === "block") {
+        n.style.display = "none";
+    } else {
+        n.style.display = "block"
+    }
+}
